f001eee29182592019044e84e406cfe1b4c14e9a
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2024-04-06 14:41:23 +0300

    fix: rework svg to jsx

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,7 +1,8 @@
 import { ComponentProps, ComponentPropsWithoutRef } from 'react'
 
 import s from './table.module.scss'
-import { ArrowMiniDownIcon, ArrowMiniUpIcon } from "@/assets";
+
+import { ArrowMiniDownIcon, ArrowMiniUpIcon } from '@/assets'
 
 type TableProps = ComponentProps<'table'>
 export const Table = ({ className, ...rest }: TableProps) => {
@@ -68,11 +69,19 @@ export const TableHeader = ({
     <TableHead {...restProps}>
       <TableRow>
         {columns.map(({ title, key, sortable = true }) => (
-          <TableHeaderData key={key} onClick={handleSort(key, sortable)} style={{height:'36px', overflow:'hidden'}}>
-            <div style={{ display: "flex", justifyContent: "start", maxHeight:'24px' }}>{title}
-              {sort && sort.key === key &&
-                <span>{sort.direction === "asc" ?
-                  <ArrowMiniUpIcon /> : <ArrowMiniDownIcon />}</span>}</div>
+          <TableHeaderData
+            key={key}
+            onClick={handleSort(key, sortable)}
+            style={{ height: '36px', overflow: 'hidden' }}
+          >
+            <div style={{ display: 'flex', justifyContent: 'start', maxHeight: '24px' }}>
+              {title}
+              {sort && sort.key === key && (
+                <span>
+                  {sort.direction === 'asc' ? <ArrowMiniUpIcon /> : <ArrowMiniDownIcon />}
+                </span>
+              )}
+            </div>
             {/*{sort && sort.key === key && <span>{sort.direction === 'asc' ? '▲' : '▼'}</span>}*/}
           </TableHeaderData>
         ))}
